0344e0aa87ce0e7651fb5fbbcc775bb815f0f250
Ninon <114509330+Nap362@users.noreply.github.com>
2024-03-17 14:23:46 +0100

    Add files via upload

--- TP_Test/src/test/Mutants.java
+++ TP_Test/src/test/Mutants.java
@@ -19,20 +19,10 @@ public class Mutants {
 			anagramme = mutant3(anagramme);
 			break;
 		}
-		case 4: {
+		default: {
 			anagramme = mutant4(anagramme);
 			break;
 		}
-		case 5: {
-			anagramme = mutant5(anagramme);
-			break;
-		}
-		case 6: {
-			anagramme = mutant6(anagramme);
-			break;
-		}
-		default:
-			anagramme = mutant7(anagramme);
 		}
 		return anagramme;
 	}
@@ -86,54 +76,6 @@ public class Mutants {
 	}
 	
 	public char[] mutant4(char[] anagramme) {
-		int taille_anagramme = anagramme.length;
-		char car;
-		for (int i = 0; i <= taille_anagramme - 1; i++) {
-			car = anagramme[i];
-			int j = i;
-			//changement - en +
-			while ((j>0) && (car<anagramme[j+1])) {
-				anagramme[j] = anagramme[j-1];
-				j--;
-			}
-			anagramme[j] = car;
-		}
-		return anagramme;
-	}
-	
-	public char[] mutant5(char[] anagramme) {
-		int taille_anagramme = anagramme.length;
-		char car;
-		for (int i = 0; i <= taille_anagramme - 1; i++) {
-			car = anagramme[i];
-			int j = i;
-			while ((j>0) && (car<anagramme[j-1])) {
-				//changement - en +
-				anagramme[j] = anagramme[j+1];
-				j--;
-			}
-			anagramme[j] = car;
-		}
-		return anagramme;
-	}
-	
-	public char[] mutant6(char[] anagramme) {
-		int taille_anagramme = anagramme.length;
-		char car;
-		for (int i = 0; i <= taille_anagramme - 1; i++) {
-			car = anagramme[i];
-			int j = i;
-			// changement > en >=
-			while ((j>=0) && (car<anagramme[j-1])) {
-				anagramme[j] = anagramme[j-1];
-				j--;
-			}
-			anagramme[j] = car;
-		}
-		return anagramme;
-	}
-	
-	public char[] mutant7(char[] anagramme) {
 		int taille_anagramme = anagramme.length;
 		char car;
 		for (int i = 0; i <= taille_anagramme - 1; i++) {
