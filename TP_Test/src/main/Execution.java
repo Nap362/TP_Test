d5679b2d2ae51f1b6257662fe4c55c1e6500f9dd
Noémie Gireaud <noemie.gireaud@gmail.com>
2024-04-08 15:16:41 +0200

    finalisation du code - ajout tests fonctionnels

--- TP_Test/src/main/Execution.java
+++ TP_Test/src/main/Execution.java
@@ -8,20 +8,11 @@ import test.JeuTest;
 
 public class Execution {
 	public static void main(String[] args) {
-		// Tests
-		JeuTest test = new JeuTest();
-		test.testAnagaramme();
-		test.testMutants();
-		
+
 		Anagramme anagramme = new Anagramme(new Scanner(System.in));
-		// Test entrée utilisateur
+		// Entrée utilisateur
 		System.out.println("\nEntrée utilisateur : ");
 		char[] mot = anagramme.recupererEntree();
 		System.out.println(anagramme.constructionAnagramme(mot));
-		
-		// Test coverage
-//		mot = "mouton".toCharArray();
-//		System.out.println("mouton -> " + String.copyValueOf(anagramme.constructionAnagramme(mot)));
-		
 	}
 }
--- TP_Test/src/test/JeuTest.java
+++ TP_Test/src/test/JeuTest.java
@@ -1,10 +1,9 @@
 package test;
 
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.NoSuchElementException;
@@ -13,56 +12,75 @@ import java.util.Scanner;
 import main.Anagramme;
 
 public class JeuTest {
-	private ArrayList<String> cas;
-	private ArrayList<String> oracles;
-	
-	public JeuTest() {
+	private static ArrayList<String> cas;
+	private static ArrayList<String> oracles;
+
+	public static void lectureFichier(String nom_jeu_test) {
 		cas = new ArrayList<>();
 		oracles = new ArrayList<>();
-		lectureFichier();
-	}
-	
-	public void lectureFichier() {
-		Path currentDirectoryPath = FileSystems.getDefault().getPath("");
-		String currentDirectoryName = currentDirectoryPath.toAbsolutePath().toString();
+		
+		String line = null;
+		BufferedReader br = null;
 		try {
-			FileReader fileReader = new FileReader(currentDirectoryName + "/src/valeurs_test.txt");
-			Scanner scanner = new Scanner(fileReader);
-			String line = scanner.nextLine();
-			String[] lineSplit;
-			while (line!=null) {
-				lineSplit = line.split(";");
-				cas.add(lineSplit[0]);
-				oracles.add(lineSplit[1]);
-				try {
-					line = scanner.nextLine();
+			FileReader fileReader = new FileReader( "TP_Test/src/test/" + nom_jeu_test);
+			br = new BufferedReader(fileReader);
+			while ((line = br.readLine()) != null) {
+	 			try {
+					String[] lineSplit = line.split(";");
+					if(lineSplit.length == 2 ) {
+						cas.add(lineSplit[0]);
+						oracles.add(lineSplit[1]);
+					}
+					else {
+						cas.add(line);
+						oracles.add("");
+					}
 				} catch (NoSuchElementException e) {
 					line = null;
 				} 
 			}
-			scanner.close();
-			fileReader.close();
+			br.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public void testAnagaramme() {
-		System.out.println("Tests fonctionnels :");
+	public static void testAnagaramme() {
+		lectureFichier("valeurs_test.txt");
+		System.out.println("Tests :");
 		Anagramme anagramme = new Anagramme(new Scanner(System.in));
 		for (int i = 0; i<cas.size();i++) {
 			char[] c = cas.get(i).toCharArray();
 			char[] oracle = oracles.get(i).toCharArray();
 			char[] res = anagramme.constructionAnagramme(c);
 			if (Arrays.equals(oracle, res)) {
-				System.out.println("Test " + (i+1) + " passed -> \"" + cas.get(i) + "\" : \"" + String.copyValueOf(oracle) + "\"");
+				System.out.println("Test " + (i+1) + " passed -> \"" + cas.get(i) 
+				+ "\" : \"" + String.copyValueOf(oracle) + "\"");
 			}else {
-				System.out.println("Test " + (i+1) + " failed -> \"" + cas.get(i) + "\" expected \"" + String.copyValueOf(oracle) + "\" but was \"" + String.copyValueOf(res) + "\"");
+				System.out.println("Test " + (i+1) + " failed -> \"" + cas.get(i) +
+						"\" expected \"" + String.copyValueOf(oracle) + "\" but was \"" + String.copyValueOf(res) + "\"");
 			}
 		}
 	}
 	
-	public void testMutants() {
+	public static void testFonctionnels() {
+		lectureFichier("tests_fonctionnels.txt");
+		System.out.println("\nTests Fonctionnels:");
+		Anagramme anagramme = new Anagramme(new Scanner(System.in));
+		for (int i = 0; i<cas.size();i++) {
+			char[] c = cas.get(i).toCharArray();
+			char[] res = anagramme.constructionAnagramme(c);
+			try {
+				System.out.println("Test " + (i+1) + " : \"" + cas.get(i) + "\" -> \"" + String.copyValueOf(res) + "\"");
+			}
+			catch(Exception e) {
+				System.out.println("Test " + (i+1) + " : \"" + cas.get(i) + "\" -> failed : error ");
+			}
+		}
+	}
+	
+	public static void testMutants() {
+		lectureFichier("valeurs_test.txt");
 		System.out.println("\nTests de mutation :");
 		Mutants mutants;
 		for (int j = 1; j<=4;j++) {
@@ -88,6 +106,17 @@ public class JeuTest {
 				System.out.println("Mutant " + j + " killed");
 			}
 		}
+	}
+	
+	public static void main(String[] args) {
+		// Test coverage
+		//char[] mot = "mouton".toCharArray();
+		//System.out.println("mouton -> " + String.copyValueOf(new Anagramme(null).constructionAnagramme(mot)));
+		
+		// Tests
+		testAnagaramme();
+		testFonctionnels();
+		testMutants();		
 	}
 	
 	
